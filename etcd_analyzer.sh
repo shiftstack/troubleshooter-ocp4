ef93d762d6186919412d74286f4ff81113afcf8f
peter daemonna ducai <peter.ducai@gmail.com>
2022-09-21 15:09:46 +0200

    dates

--- etcd_analyzer.sh
+++ etcd_analyzer.sh
@@ -9,9 +9,10 @@ ETCD2=$(oc --as system:admin -n openshift-etcd get -l k8s-app=etcd pods|tail -3|
 # oc exec $(oc --as system:admin -n openshift-etcd get -l k8s-app=etcd pods|tail -3|head -1| cut -d ' ' -f1)  -c etcdctl -n openshift-etcd --  etcdctl watch / --prefix  --write-out=fields
 
 
-#curl -k https://api.<OCP URL>.com -w "%{time_connect}\n"
 
-echo -e "[$ETCD0]"
+echo -e ""
+echo -e "-[$ETCD0]--------------------"
+
 echo -e ""
 oc exec $ETCD0 -c etcdctl -- etcdctl endpoint status -w table
 echo -e "IPs:"
@@ -19,13 +20,18 @@ oc exec $ETCD0 -c etcd -- ip a s dev br-ex|grep inet
 echo -e "Errors and dropped packets:"
 oc exec $ETCD0 -c etcd -- ip -s link show dev br-ex
 echo -e ""
+echo -e "Latency against API is $(curl -k https://api.$(echo $ETCD0| sed 's/.*://').com -w "%{time_connect}\n"|tail -1)"
+echo -e ""
+echo -e "LOGS \nstart on $(oc logs $ETCD0 -c etcd|head -60|tail -1|cut -d ':' -f3|cut -c 2-14)"
+echo -e "ends on $(oc logs $ETCD0 -c etcd|tail -1|cut -d ':' -f3|cut -c 2-14)"
 echo -e "Found $(oc logs $ETCD0 -c etcd|grep overloaded|wc -l) overloaded messages"
 echo -e "Found $(oc logs $ETCD0 -c etcd|grep 'took too long'|wc -l) took too long messages"
 echo -e "Found $(oc logs $ETCD0 -c etcd|grep clock|wc -l) clock difference messages"
 echo -e "Found $(oc logs $ETCD0 -c etcd|grep heatbeat|wc -l) heartbeat messages"
+echo -e ""
 echo -e "COMPACTION: \n$(oc logs $ETCD0 -c etcd|grep compaction|tail -8|cut -d ':' -f10|cut -c 2-12)"
 echo -e ""
-echo -e "[$ETCD1]"
+echo -e "-[$ETCD1]--------------------"
 echo -e ""
 oc exec $ETCD1 -c etcdctl -- etcdctl endpoint status -w table
 echo -e ""
@@ -34,12 +40,16 @@ oc exec $ETCD1 -c etcd -- ip a s dev br-ex|grep inet
 echo -e "Errors and dropped packets:"
 oc exec $ETCD1 -c etcd -- ip -s link show dev br-ex
 echo -e ""
+echo -e "Latency against API is $(curl -k https://api.$(echo $ETCD1| sed 's/.*://').com -w "%{time_connect}\n"|tail -1)"
+echo -e ""
 echo -e "Found $(oc logs $ETCD1 -c etcd|grep overloaded|wc -l) overloaded messages"
 echo -e "Found $(oc logs $ETCD1 -c etcd|grep 'took too long'|wc -l) took too long messages"
 echo -e "Found $(oc logs $ETCD1 -c etcd|grep clock|wc -l) clock difference messages"
+echo -e "Found $(oc logs $ETCD1 -c etcd|grep heatbeat|wc -l) heartbeat messages"
+echo -e ""
 echo -e "COMPACTION: \n$(oc logs $ETCD1 -c etcd|grep compaction|tail -8|cut -d ':' -f10|cut -c 2-12)"
 echo -e ""
-echo -e "[$ETCD2]"
+echo -e "-[$ETCD2]--------------------"
 echo -e ""
 oc exec $ETCD2 -c etcdctl -- etcdctl endpoint status -w table
 echo -e ""
@@ -48,9 +58,13 @@ oc exec $ETCD2 -c etcd -- ip a s dev br-ex|grep inet
 echo -e "Errors and dropped packets:"
 oc exec $ETCD2 -c etcd -- ip -s link show dev br-ex
 echo -e ""
+echo -e "Latency against API is $(curl -k https://api.$(echo $ETCD2| sed 's/.*://').com -w "%{time_connect}\n"|tail -1)"
+echo -e ""
 echo -e "Found $(oc logs $ETCD2 -c etcd|grep overloaded|wc -l) overloaded messages"
 echo -e "Found $(oc logs $ETCD2 -c etcd|grep 'took too long'|wc -l) took too long messages"
 echo -e "Found $(oc logs $ETCD2 -c etcd|grep clock|wc -l) clock difference messages"
+echo -e "Found $(oc logs $ETCD2 -c etcd|grep heatbeat|wc -l) heartbeat messages"
+echo -e ""
 echo -e "COMPACTION: \n$(oc logs $ETCD2 -c etcd|grep compaction|tail -8|cut -d ':' -f10|cut -c 2-12)"
 echo -e ""
 
