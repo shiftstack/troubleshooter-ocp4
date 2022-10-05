0323f7d342d5760c506cd96cd24c8ff337029700
Matthew Booth <mbooth@redhat.com>
2022-10-05 11:13:12 +0100

    Use explicit namespace in etcd_analyzer.sh commands

--- etcd_analyzer.sh
+++ etcd_analyzer.sh
@@ -10,87 +10,87 @@ echo -e ""
 echo -e "-[${ETCD[0]}]--------------------"
 
 echo -e ""
-oc exec ${ETCD[0]} -c etcdctl -- etcdctl endpoint status -w table
+oc exec -n openshift-etcd ${ETCD[0]} -c etcdctl -- etcdctl endpoint status -w table
 echo -e "IPs:"
-for i in $(oc exec ${ETCD[0]} -c etcd -- ls /sys/class/net|grep -v veth|grep -v lo); do echo $i && oc exec ${ETCD[0]} -c etcd -- ip a s|grep inet|grep -v inet6|grep -v '127.'|head -2; done
+for i in $(oc exec -n openshift-etcd ${ETCD[0]} -c etcd -- ls /sys/class/net|grep -v veth|grep -v lo); do echo $i && oc exec -n openshift-etcd ${ETCD[0]} -c etcd -- ip a s|grep inet|grep -v inet6|grep -v '127.'|head -2; done
 echo -e "Errors and dropped packets:"
-for i in $(oc exec ${ETCD[0]} -c etcd -- ls /sys/class/net|grep -v veth|grep -v lo); do oc exec ${ETCD[0]} -c etcd -- ip -s link show dev $i; done
+for i in $(oc exec -n openshift-etcd ${ETCD[0]} -c etcd -- ls /sys/class/net|grep -v veth|grep -v lo); do oc exec -n openshift-etcd ${ETCD[0]} -c etcd -- ip -s link show dev $i; done
 echo -e ""
 echo -e "Latency against API is $(curl -k https://api.$(echo ${ETCD[0]}| sed 's/.*://').com -w "%{time_connect}\n"|tail -1)"
 echo -e ""
-echo -e "LOGS \nstart on $(oc logs ${ETCD[0]} -c etcd|head -60|tail -1|cut -d ':' -f3|cut -c 2-14)"
-echo -e "ends on $(oc logs ${ETCD[0]} -c etcd|tail -1|cut -d ':' -f3|cut -c 2-14)"
+echo -e "LOGS \nstart on $(oc logs -n openshift-etcd ${ETCD[0]} -c etcd|head -60|tail -1|cut -d ':' -f3|cut -c 2-14)"
+echo -e "ends on $(oc logs -n openshift-etcd ${ETCD[0]} -c etcd|tail -1|cut -d ':' -f3|cut -c 2-14)"
 echo -e ""
-echo -e "Found $(oc logs ${ETCD[0]} -c etcd|grep overloaded|wc -l) overloaded messages"
-echo -e "Found $(oc logs ${ETCD[0]} -c etcd|grep 'took too long'|wc -l) took too long messages"
-echo -e "Found $(oc logs ${ETCD[0]} -c etcd|grep clock|wc -l) clock difference messages"
-echo -e "Found $(oc logs ${ETCD[0]} -c etcd|grep heartbeat|wc -l) heartbeat messages"
-echo -e "Found $(oc logs ${ETCD[0]} -c etcd|grep 'database space exceeded'|wc -l) database space exceeded messages"
-echo -e "Found $(oc logs ${ETCD[0]} -c etcd|grep 'leader changed'|wc -l) leader changed messages"
+echo -e "Found $(oc logs -n openshift-etcd ${ETCD[0]} -c etcd|grep overloaded|wc -l) overloaded messages"
+echo -e "Found $(oc logs -n openshift-etcd ${ETCD[0]} -c etcd|grep 'took too long'|wc -l) took too long messages"
+echo -e "Found $(oc logs -n openshift-etcd ${ETCD[0]} -c etcd|grep clock|wc -l) clock difference messages"
+echo -e "Found $(oc logs -n openshift-etcd ${ETCD[0]} -c etcd|grep heartbeat|wc -l) heartbeat messages"
+echo -e "Found $(oc logs -n openshift-etcd ${ETCD[0]} -c etcd|grep 'database space exceeded'|wc -l) database space exceeded messages"
+echo -e "Found $(oc logs -n openshift-etcd ${ETCD[0]} -c etcd|grep 'leader changed'|wc -l) leader changed messages"
 echo -e ""
-echo -e "COMPACTION: \n$(oc logs ${ETCD[0]} -c etcd|grep compaction|tail -8|cut -d ':' -f10|cut -c 2-12)"
+echo -e "COMPACTION: \n$(oc logs -n openshift-etcd ${ETCD[0]} -c etcd|grep compaction|tail -8|cut -d ':' -f10|cut -c 2-12)"
 echo -e ""
 echo -e "-[${ETCD[1]}]--------------------"
 echo -e ""
-oc exec ${ETCD[1]} -c etcdctl -- etcdctl endpoint status -w table
+oc exec -n openshift-etcd ${ETCD[1]} -c etcdctl -- etcdctl endpoint status -w table
 echo -e ""
 echo -e "IPs:"
-for i in $(oc exec ${ETCD[1]} -c etcd -- ls /sys/class/net|grep -v veth|grep -v lo); do echo $i && oc exec ${ETCD[1]} -c etcd -- ip a s|grep inet|grep -v inet6|grep -v '127.'|head -2; done
+for i in $(oc exec -n openshift-etcd ${ETCD[1]} -c etcd -- ls /sys/class/net|grep -v veth|grep -v lo); do echo $i && oc exec -n openshift-etcd ${ETCD[1]} -c etcd -- ip a s|grep inet|grep -v inet6|grep -v '127.'|head -2; done
 echo -e "Errors and dropped packets:"
-for i in $(oc exec ${ETCD[1]} -c etcd -- ls /sys/class/net|grep -v veth|grep -v lo); do oc exec ${ETCD[1]} -c etcd -- ip -s link show dev $i; done
+for i in $(oc exec -n openshift-etcd ${ETCD[1]} -c etcd -- ls /sys/class/net|grep -v veth|grep -v lo); do oc exec -n openshift-etcd ${ETCD[1]} -c etcd -- ip -s link show dev $i; done
 echo -e ""
 echo -e "Latency against API is $(curl -k https://api.$(echo ${ETCD[1]}| sed 's/.*://').com -w "%{time_connect}\n"|tail -1)"
 echo -e ""
-echo -e "LOGS \nstart on $(oc logs ${ETCD[1]} -c etcd|head -60|tail -1|cut -d ':' -f3|cut -c 2-14)"
-echo -e "ends on $(oc logs ${ETCD[1]} -c etcd|tail -1|cut -d ':' -f3|cut -c 2-14)"
+echo -e "LOGS \nstart on $(oc logs -n openshift-etcd ${ETCD[1]} -c etcd|head -60|tail -1|cut -d ':' -f3|cut -c 2-14)"
+echo -e "ends on $(oc logs -n openshift-etcd ${ETCD[1]} -c etcd|tail -1|cut -d ':' -f3|cut -c 2-14)"
 echo -e ""
-echo -e "Found $(oc logs ${ETCD[1]} -c etcd|grep overloaded|wc -l) overloaded messages"
-echo -e "Found $(oc logs ${ETCD[1]} -c etcd|grep 'took too long'|wc -l) took too long messages"
-echo -e "Found $(oc logs ${ETCD[1]} -c etcd|grep clock|wc -l) clock difference messages"
-echo -e "Found $(oc logs ${ETCD[1]} -c etcd|grep heartbeat|wc -l) heartbeat messages"
-echo -e "Found $(oc logs ${ETCD[1]} -c etcd|grep 'database space exceeded'|wc -l) database space exceeded messages"
-echo -e "Found $(oc logs ${ETCD[1]} -c etcd|grep 'leader changed'|wc -l) leader changed messages"
+echo -e "Found $(oc logs -n openshift-etcd ${ETCD[1]} -c etcd|grep overloaded|wc -l) overloaded messages"
+echo -e "Found $(oc logs -n openshift-etcd ${ETCD[1]} -c etcd|grep 'took too long'|wc -l) took too long messages"
+echo -e "Found $(oc logs -n openshift-etcd ${ETCD[1]} -c etcd|grep clock|wc -l) clock difference messages"
+echo -e "Found $(oc logs -n openshift-etcd ${ETCD[1]} -c etcd|grep heartbeat|wc -l) heartbeat messages"
+echo -e "Found $(oc logs -n openshift-etcd ${ETCD[1]} -c etcd|grep 'database space exceeded'|wc -l) database space exceeded messages"
+echo -e "Found $(oc logs -n openshift-etcd ${ETCD[1]} -c etcd|grep 'leader changed'|wc -l) leader changed messages"
 echo -e ""
-echo -e "COMPACTION: \n$(oc logs ${ETCD[1]} -c etcd|grep compaction|tail -8|cut -d ':' -f10|cut -c 2-12)"
+echo -e "COMPACTION: \n$(oc logs -n openshift-etcd ${ETCD[1]} -c etcd|grep compaction|tail -8|cut -d ':' -f10|cut -c 2-12)"
 echo -e ""
 echo -e "-[echo ${ETCD[2]}]--------------------"
 echo -e ""
-oc exec ${ETCD[2]} -c etcdctl -- etcdctl endpoint status -w table
+oc exec -n openshift-etcd ${ETCD[2]} -c etcdctl -- etcdctl endpoint status -w table
 echo -e ""
 echo -e "IPs:"
-for i in $(oc exec ${ETCD[2]} -c etcd -- ls /sys/class/net|grep -v veth|grep -v lo); do echo $i && oc exec ${ETCD[2]} -c etcd -- ip a s|grep inet|grep -v inet6|grep -v '127.'|head -2; done
+for i in $(oc exec -n openshift-etcd ${ETCD[2]} -c etcd -- ls /sys/class/net|grep -v veth|grep -v lo); do echo $i && oc exec -n openshift-etcd ${ETCD[2]} -c etcd -- ip a s|grep inet|grep -v inet6|grep -v '127.'|head -2; done
 echo -e "Errors and dropped packets:"
-for i in $(oc exec ${ETCD[2]} -c etcd -- ls /sys/class/net|grep -v veth|grep -v lo); do oc exec ${ETCD[2]} -c etcd -- ip -s link show dev $i; done
+for i in $(oc exec -n openshift-etcd ${ETCD[2]} -c etcd -- ls /sys/class/net|grep -v veth|grep -v lo); do oc exec -n openshift-etcd ${ETCD[2]} -c etcd -- ip -s link show dev $i; done
 echo -e ""
 echo -e "Latency against API is $(curl -k https://api.$(echo ${ETCD[2]}| sed 's/.*://').com -w "%{time_connect}\n"|tail -1)"
 echo -e ""
-echo -e "LOGS \nstart on $(oc logs ${ETCD[2]} -c etcd|head -60|tail -1|cut -d ':' -f3|cut -c 2-14)"
-echo -e "ends on $(oc logs ${ETCD[2]} -c etcd|tail -1|cut -d ':' -f3|cut -c 2-14)"
+echo -e "LOGS \nstart on $(oc logs -n openshift-etcd ${ETCD[2]} -c etcd|head -60|tail -1|cut -d ':' -f3|cut -c 2-14)"
+echo -e "ends on $(oc logs -n openshift-etcd ${ETCD[2]} -c etcd|tail -1|cut -d ':' -f3|cut -c 2-14)"
 echo -e ""
-echo -e "Found $(oc logs ${ETCD[2]} -c etcd|grep overloaded|wc -l) overloaded messages"
-echo -e "Found $(oc logs ${ETCD[2]} -c etcd|grep 'took too long'|wc -l) took too long messages"
-echo -e "Found $(oc logs ${ETCD[2]} -c etcd|grep clock|wc -l) clock difference messages"
-echo -e "Found $(oc logs ${ETCD[2]} -c etcd|grep heartbeat|wc -l) heartbeat messages"
-echo -e "Found $(oc logs ${ETCD[2]} -c etcd|grep 'database space exceeded'|wc -l) database space exceeded messages"
-echo -e "Found $(oc logs ${ETCD[2]} -c etcd|grep 'leader changed'|wc -l) leader changed messages"
+echo -e "Found $(oc logs -n openshift-etcd ${ETCD[2]} -c etcd|grep overloaded|wc -l) overloaded messages"
+echo -e "Found $(oc logs -n openshift-etcd ${ETCD[2]} -c etcd|grep 'took too long'|wc -l) took too long messages"
+echo -e "Found $(oc logs -n openshift-etcd ${ETCD[2]} -c etcd|grep clock|wc -l) clock difference messages"
+echo -e "Found $(oc logs -n openshift-etcd ${ETCD[2]} -c etcd|grep heartbeat|wc -l) heartbeat messages"
+echo -e "Found $(oc logs -n openshift-etcd ${ETCD[2]} -c etcd|grep 'database space exceeded'|wc -l) database space exceeded messages"
+echo -e "Found $(oc logs -n openshift-etcd ${ETCD[2]} -c etcd|grep 'leader changed'|wc -l) leader changed messages"
 echo -e ""
-echo -e "COMPACTION: \n$(oc logs ${ETCD[2]} -c etcd|grep compaction|tail -8|cut -d ':' -f10|cut -c 2-12)"
+echo -e "COMPACTION: \n$(oc logs -n openshift-etcd ${ETCD[2]} -c etcd|grep compaction|tail -8|cut -d ':' -f10|cut -c 2-12)"
 echo -e ""
 
 echo -e ""
 echo -e "[NUMBER OF OBJECTS IN ETCD]"
 echo -e ""
-oc exec ${ETCD[0]} -c etcdctl -- etcdctl get / --prefix --keys-only | sed '/^$/d' | cut -d/ -f3 | sort | uniq -c | sort -rn|head -14
+oc exec -n openshift-etcd ${ETCD[0]} -c etcdctl -- etcdctl get / --prefix --keys-only | sed '/^$/d' | cut -d/ -f3 | sort | uniq -c | sort -rn|head -14
 echo -e ""
 
 echo -e "[BIGGEST CONSUMERS]"
 echo -e ""
-oc exec ${ETCD[0]} -c etcdctl -n openshift-etcd -- etcdctl get / --prefix --keys-only > keysonly.txt
+oc exec -n openshift-etcd ${ETCD[0]} -c etcdctl -n openshift-etcd -- etcdctl get / --prefix --keys-only > keysonly.txt
 cat keysonly.txt | grep event |cut -d/ -f3,4| sort | uniq -c | sort -n --rev |head -10
 echo -e "..."
 cat keysonly.txt | grep event |cut -d/ -f3,4,5| sort | uniq -c | sort -n --rev |head -10
 
-# oc exec ${ETCD[0]}  -c etcdctl -n openshift-etcd --  etcdctl watch / --prefix  --write-out=fields > fields.txt
+# oc exec -n openshift-etcd ${ETCD[0]}  -c etcdctl -n openshift-etcd --  etcdctl watch / --prefix  --write-out=fields > fields.txt
 
 
 
